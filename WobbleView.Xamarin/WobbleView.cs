07c102f760162b543ec160b731eee4f50da7e937
Timo Salomäki <timo@subsonicdesign.net>
2019-06-22 11:08:40 +0200

    Initial commit

---
+++ WobbleView.Xamarin/WobbleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Foundation;
+using UIKit;
+
+namespace WobbleView.Xamarin
+{
+    [Register("WobbleView"), DesignTimeVisible(true)]
+    public class WobbleView : UIView
+    {
+    }
+}
